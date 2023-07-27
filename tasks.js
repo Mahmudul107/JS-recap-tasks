00f2372e17c2953280976ac2be57366cc89b6c5a
Mahmudul107 <mahmudulislam378@gmail.com>
2023-07-27 12:23:17 +0600

    calculator with js

--- tasks.js
+++ tasks.js
@@ -50,8 +50,6 @@ const inputArray = [3, 5, 7, 5, 3, 3, 8, 4, 5, 6, 6];
 const mostFrequent = findMostFrequentElement(inputArray);
 console.log(mostFrequent);
 
-
-
 // Task 4: Create a function that takes a sorted array of numbers and a target value as input. The function should find two numbers in the array that add up to the target value. Return an array containing the indices of the two numbers.
 
 function findTwoNumbersSum(arr, target) {
@@ -78,6 +76,42 @@ const targetValue = 16;
 // const indices = findTwoNumbersSum(sortedArray, targetValue);
 console.log(findTwoNumbersSum(sortedArray, targetValue));
 
+
+
+// Task 5: Implement a simple JavaScript calculator. The calculator should take two numbers and an operator (+, -, *, /) as input and return the result of the operation.
+
+function calculator(num1, operator, num2) {
+  // Check if the operator is addition (+)
+  if (operator === "+") {
+    return num1 + num2;
+  }
+  // Check if the operator is subtraction (-)
+  else if (operator === "-") {
+    return num1 - num2;
+  }
+  // Check if the operator is multiplication (*)
+  else if (operator === "*") {
+    return num1 * num2;
+  }
+  // Check if the operator is division (/)
+  else if (operator === "/") {
+    // Check if num2 is zero to prevent division by zero
+    if (num2 === 0) {
+      return "Cannot divide by zero";
+    }
+    return num1 / num2;
+  }
+  // If the operator is not recognized, return an error message
+  else {
+    return "Invalid operator";
+  }
+}
+
+console.log(calculator(5, "+", 3)); // Output: 8
+console.log(calculator(10, "-", 4)); // Output: 6
+console.log(calculator(3, "*", 6)); // Output: 18
+console.log(calculator(8, "/", 2)); // Output: 4
+console.log(calculator(5, "%", 3));
 
 
 // Task 8: Implement a JavaScript function to find the second smallest element in an array of numbers. The function should return the second smallest number.
