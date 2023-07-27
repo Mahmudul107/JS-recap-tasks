d3de7e3ea0cb65b73fadf7568463d47dd7056c3b
Mahmudul107 <mahmudulislam378@gmail.com>
2023-07-27 10:19:15 +0600

    found sum of  postive numbers

--- tasks.js
+++ tasks.js
@@ -1,4 +1,4 @@
-// Task 1:Create a function that takes a string as input and returns the reversed version of the string without using the built-in reverse() method. 
+// Task 1: Create a function that takes a string as input and returns the reversed version of the string without using the built-in reverse() method. 
 
 function reverseString(string) {
     return string.split('').reverse().join('');
@@ -6,3 +6,23 @@ function reverseString(string) {
 const string = "Let's reverse the string";
 const reversedString = reverseString(string);
 console.log(reversedString);
+
+
+
+// Task 2: Create a function that takes an array of numbers as input and returns the sum of all positive numbers in the array. 
+
+function positiveNumbers(arr){
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] > 0){
+            sum += arr[i];
+        }
+    }
+    return sum;
+}
+
+const arrayOfNumbers = [2, -5, 10, -3, 7];
+const positiveNumberOfNumbers = positiveNumbers(arrayOfNumbers);
+console.log(positiveNumberOfNumbers);
+
+
